b0d73eda82bac41bd1376b793282224dc3c89587
VarNotUsed <varnotused@protonmail.com>
2023-09-26 11:58:44 +0200

    removed the disable attribute and set link to app

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { Button as MuiButton, Tooltip } from "@mui/material";
+import { Button as MuiButton } from "@mui/material";
 
 type ButtonProps = {
   label: string;
@@ -6,37 +6,39 @@ type ButtonProps = {
   style?: any;
 };
 
+const openLink = () => {
+  const appLink = "https://app.phoenix-hub.io/";
+
+  window.open(appLink, "_blank");
+};
+
 const Button = ({ label, type, style }: ButtonProps) => {
-  //temporarly disable launch app buttons
-  const disabled = type != "secondary";
+  const isLaunchButton = label == "Launch App";
 
   return (
-    <Tooltip arrow title={disabled ? "Coming soon!" : ""}>
-      <MuiButton
-        sx={{
-          color: "#FFF",
-          padding: "16px 38px",
-          fontFamily: "Ubuntu",
-          textTransform: "none",
-          cursor: disabled ? "default" : "pointer",
-          opacity: disabled ? "0.6": "1",
-          fontSize: {
-            xs: "16px",
-            md: "14px",
-          },
-          fontStyle: "normal",
-          fontWeight: 700,
-          lineHeight: "20px",
-          borderRadius: "16px",
-          background: type != "secondary" ? 
-            "linear-gradient(135deg, #E2491A 0%, #E21B1B 17.08%, #E2491A 42.71%, #E2AA1B 100%)" 
-            : "linear-gradient(180deg, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0.03) 100%)",
-          ...style,
-        }}
-      >
-        {label}
-      </MuiButton>
-    </Tooltip>
+    <MuiButton
+      onClick={isLaunchButton ? openLink : undefined}
+      sx={{
+        color: "#FFF",
+        padding: "16px 38px",
+        fontFamily: "Ubuntu",
+        textTransform: "none",
+        fontSize: {
+          xs: "16px",
+          md: "14px",
+        },
+        fontStyle: "normal",
+        fontWeight: 700,
+        lineHeight: "20px",
+        borderRadius: "16px",
+        background: type != "secondary" ? 
+          "linear-gradient(135deg, #E2491A 0%, #E21B1B 17.08%, #E2491A 42.71%, #E2AA1B 100%)" 
+          : "linear-gradient(180deg, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0.03) 100%)",
+        ...style,
+      }}
+    >
+      {label}
+    </MuiButton>
   );
 };
 
