d373575f36f1000c2191dd3ab0547a5a54bd7b7c
VarNotUsed <varnotused@protonmail.com>
2024-02-07 16:30:40 +0100

    meta data

--- src/components/Seo/index.tsx
+++ src/components/Seo/index.tsx
@@ -20,8 +20,36 @@ const Seo = () => {
       ></link>
       <meta name="robots" content="index,follow" />
       <meta property="og:locale" content="en" />
-      <meta property="og:title" content="Phoenix" />
       <meta property="og:site_name" content="Phoenix" />
+
+      <meta
+        name="description"
+        content="Explore Phoenix DeFi Hub on Soroban - your gateway to innovative decentralized finance solutions. Experience seamless, secure, and advanced DeFi services with cutting-edge smart contract capabilities. Join the future of finance today."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://phoenix-hub.io" />
+      <meta property="og:title" content="Phoenix DeFi Hub on Soroban" />
+      <meta
+        property="og:description"
+        content="Explore Phoenix DeFi Hub on Soroban - your gateway to innovative decentralized finance solutions. Experience seamless, secure, and advanced DeFi services with cutting-edge smart contract capabilities. Join the future of finance today."
+      />
+      <meta
+        property="og:image"
+        content="https://app.phoenix-hub.io//socials.png"
+      />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:url" content="https://app.phoenix-hub.io" />
+      <meta name="twitter:title" content="Phoenix DeFi Hub on Soroban" />
+      <meta
+        name="twitter:description"
+        content="Explore Phoenix DeFi Hub on Soroban - your gateway to innovative decentralized finance solutions. Experience seamless, secure, and advanced DeFi services with cutting-edge smart contract capabilities. Join the future of finance today."
+      />
+      <meta
+        name="twitter:image"
+        content="https://app.phoenix-hub.io/socials-square.png"
+      />
       <title>Phoenix</title>
     </Helmet>
   );
